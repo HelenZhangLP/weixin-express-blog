b87705bc6e58019e4b6df487e07c0455b2e4a222
lipingzhang_i567 <lipingzhang_ajk@58ganji.com>
2018-01-26 20:54:46 +0800

    fn:message-packaging

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express'),
-		crypto = require('crypto'),
 		parser = require('xml2json'),
 		router = express.Router();
 
 var wechatUtils = require('../utils/wechatUtils'),
 		weChat = require('../weixin/weChat'),
+		MessageHandle = require('../weixin/MessageHandle'),
 		wxMsg = require('../weixin/msg');
 
 /* GET home page. */
@@ -16,66 +16,21 @@ router.get('/', function(req, res, next) {
 
 //处理微信消息
 router.post('/', (req, res, next) => {
+	let MsgHandle = new MessageHandle();
 	try {
 		let dataJson;
 		req.on('data', (data) => {
 			dataJson = parser.toJson(data.toString('utf-8'),{object:true});
-			console.log('data',typeof dataJson.xml);
-			console.log('data',dataJson);
 		})
 
 		req.on('end', function(){
-			let xml = msgHandle(dataJson.xml.MsgType, dataJson.xml.Event, dataJson);
+			let xml = MsgHandle.requirestHandle(dataJson);
 			
-			console.log(xml);
 			return res.send(xml);
 		})
 	} catch(e) {
 		console.log('error:' + error);
 	} 
 })
-
-function msgHandle(mType, eType, json) {
-	if(mType == 'event') {
-		switch(eType) {
-			case 'subscribe':
-			return createResMsg(json, wxMsg.subscribe);
-			break;
-		}
-	} else if(mType == 'text') {
-		return createResMsg(json,json.xml.Content);
-	}
-}
-
-function createResMsg(json, msg){
-	var res = {},xml=[];
-  res.ToUserName = json.xml.FromUserName;
-  res.FromUserName  = json.xml.ToUserName;
-  res.CreateTime = new Date().getTime();
-  res.MsgType = 'text';
-  res.Content = msg;
-
-  xml.push("<xml>");
-  xml.push(jsonToXml(res));
-  xml.push("</xml>");
-  return xml.join('');
-}
-
-function jsonToXml(json) {
-	let xml = [];
-	for (var i in json) {
-		if (typeof json[i] == 'number') {
-			xml.push('<'+i+'>'+ json[i] +'</'+i+'>')
-		} else if(typeof json[i] == 'string') {
-			//这个 bug 值得记念，错误暂时注释掉，耗时一天。
-			//xml.push('<'+i+'>'+ '< ![CDATA['+ json[i] + '] ]>' +'</'+i+'>')
-			xml.push('<'+i+'><![CDATA['+ json[i] +']]></'+i+'>')
-		} else if(typeof json[i] === 'object'){
-        xml.push('<'+i+'>' + jsonToXml(json[i] + '</'+i+'>'));
-    }
-	}
-
-	return xml.join('');
-}
 
 module.exports = router;
